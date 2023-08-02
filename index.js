72b72aa474420dd5ca0d29a907c1152def496a54
Dean Pektas <deanpektas12345@gmail.com>
2023-08-02 17:57:49 -0400

    Changed application form link to google form as per Nicks request

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const btn = document.querySelector('#applybtn');
 btn.addEventListener( 'click', () => {
-    window.open('https://form.feathery.io/to/hJgPbJ', '_blank');
+    window.open('https://docs.google.com/forms/d/e/1FAIpQLSdDWdkC8PxOllUVBJJL0yxanAp8692KZY-l16EFJnSshwM6zQ/viewform', '_blank');
 });
