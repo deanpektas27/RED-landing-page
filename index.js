6b67f23146673f2f651ab56f8b9ab2f85b27c772
Dean Pektas <deanpektas12345@gmail.com>
2023-08-23 12:34:54 -0400

    Added second apply button to page, added event listener for all btns with same ids

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const btn = document.querySelector('#applybtn');
-btn.addEventListener( 'click', () => {
+const btn = document.querySelectorAll('#applybtn');
+btn.forEach((btn) => {
+    btn.addEventListener( 'click', () => {
     window.open('https://docs.google.com/forms/d/e/1FAIpQLSdDWdkC8PxOllUVBJJL0yxanAp8692KZY-l16EFJnSshwM6zQ/viewform', '_blank');
+    });
 });
